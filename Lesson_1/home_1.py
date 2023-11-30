8bcaf32d94db4b868264b087e35f1294345651a0
levesin <lionrav19@gmail.com>
2023-11-30 22:03:09 +0200

    home_1

---
+++ Lesson_1/home_1.py
@@ -0,0 +1,55 @@
+
+# 1 задание
+print("Задание 1")
+a = 3
+b = 3
+c = 3
+print(a == b)
+print(a == c)
+print(b == c)
+
+print(a is b)
+print(a is c)
+print(a is c)
+
+print("="*50)
+# 2 задание
+print("Задание 2")
+
+a1 = [4]
+b1 = [4]
+
+print(id(a1))
+print(id(b1))
+
+print(a1 == b1)
+print(a1 is b1)
+
+# 3 задание
+print("="*50)
+print("Задание 3(1.2)")
+
+a = [3]
+b = [3]
+c = [3]
+
+print(id(a))
+print(id(b))
+print(id(c))
+
+print(a == b)
+print(a == c)
+print(b == c)
+print("-"*50)
+print(a is b)
+print(a is c)
+print(a is c)
+
+print("="*50)
+print("Задание 3(2.2)")
+
+a1 = 4
+b1 = 4
+
+print(a1 == b1)
+print(a1 is b1)
